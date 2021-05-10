e95eed8dc2da1945cf7225b64795f78690e4e1d7
BrianTheLion328 <bmoniz328@gmail.com>
2021-05-09 20:40:11 -0700

    halfway done with module 3 (cannot get tags to show up)

--- db/seed.js
+++ db/seed.js
@@ -7,7 +7,9 @@ const {
   createPost,
   updatePost,
   getAllPosts,
-  getPostsByUser
+  getPostsByUser,
+  createTags,
+  addTagsToPost
 } = require('./index');
 
 async function dropTables() {
@@ -54,8 +56,8 @@ async function createTables() {
         name VARCHAR(255) UNIQUE NOT NULL
       );
       CREATE TABLE post_tags (
-        "postId" INTEGER REFERENCES UNIQUE posts(id),
-        "tagId" INTEGER REFERENCES UNIQUE tags(id)
+        "postId" INTEGER UNIQUE REFERENCES posts(id),
+        "tagId" INTEGER UNIQUE REFERENCES tags(id)
       );
     `);
 
@@ -176,6 +178,29 @@ async function testDB() {
   }
 }
 
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+
+    const [happy, sad, inspo, catman] = await createTags([
+      '#happy',
+      '#worst-day-ever',
+      '#youcandoanything',
+      '#catmandoeverything'
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log("Finished creating tags!");
+  } catch (error) {
+    console.log("Error creating tags!");
+    throw error;
+  }
+}
 
 rebuildDB()
   .then(testDB)
