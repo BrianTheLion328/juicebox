c4fcf0b8e966d2e42e25cf0ed1eed8872c02f2af
BrianTheLion328 <bmoniz328@gmail.com>
2021-05-15 13:58:35 -0700

    day 3 work in progress

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,72 @@
 const express = require('express');
 const postsRouter = express.Router();
+const { getAllPosts, createPost, updatePost, getPostById } = require('../db');
+const { requireUser } = require('./utils');
 
-const { getAllPosts } = require('../db');
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  let { title, content, tags = "" } = req.body;
+
+  let tagArr = tags.trim().split(/\s+/)
+   let postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData = {
+      authorId: req.user.id,
+      title: title,
+      content: content,
+      tags: postData.tags,
+    }
+    const post = await createPost(postData);
+    // if the post comes back, res.send({ post });
+    if (post){
+      res.send( {post} );
+    }
+    // otherwise, next an appropriate error object
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  let updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
